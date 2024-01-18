04c2a7a5e43365e9fe8f0cc280acb4536de5f3e4
adamcesco <acescobedo@smu.edu>
2024-01-18 16:26:29 -0600

    first page edits

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -10,60 +10,39 @@ export default function Home() {
     <>
       <AsciiFlower />
       <button type="button" id="contact-button">EMAIL</button>
-      <div id="welcome-page">
-        <div id="welcome-text-div" style={{ zIndex: '3' }}>
-          <h1 className="welcome-name" style={{ fontSize: '11.05rem', marginLeft: '-.04%' }}>
-            ADAM
-          </h1>
-          <h1 className="welcome-name" style={{ fontSize: '6rem', marginLeft: '-.94%' }}>
-            ESCOBEDO
-          </h1>
-          <h2 id="welcome-description">
-            Loves software engineering, art, and furniture design.
-            <br />
-            Click around and learn about me.
-          </h2>
-        </div>
-      </div>
-      <div
-        style={{
-          position: 'relative',
-          width: '100vw',
-          height: '5vh',
-          backgroundColor: 'rgb(89, 89, 89)',
-          top: '-1vh',
-        }}
-      />
       <div id="second-page">
-        <div id="title-container" style={{ zIndex: '3' }}>
-          <p className="title">
-            SOFTWARE ENGINEER
-          </p>
-          <p className="title">
-            EX-AMAZON INTERN
-          </p>
-          <p className="title">
-            EX-AT&amp;T INTERN
-          </p>
-          <p className="title">
-            PENDING-PATENT HOLDER
-          </p>
-          <p className="title">
-            SOUTHERN METHODIST UNIVERSITY
-          </p>
-          <p className="title">
-            BACHELOR OF SCIENCE
-          </p>
-          <p className="title">
-            COMPUTER SCIENCE
-          </p>
-          <p className="title">
-            2024 GRADUATE
-          </p>
+        <div id="name-title-container" style={{ zIndex: '3' }}>
+          <h1 id="second-page-name">Adam Escobedo</h1>
+          <div id="title-container">
+            <p className="title">
+              SOFTWARE ENGINEER
+            </p>
+            <p className="title">
+              EX-AMAZON INTERN
+            </p>
+            <p className="title">
+              EX-AT&amp;T INTERN
+            </p>
+            <p className="title">
+              PENDING-PATENT HOLDER
+            </p>
+            <p className="title">
+              SOUTHERN METHODIST UNIVERSITY
+            </p>
+            <p className="title">
+              BACHELOR OF SCIENCE
+            </p>
+            <p className="title">
+              COMPUTER SCIENCE
+            </p>
+            <p className="title">
+              2024 GRADUATE
+            </p>
+          </div>
         </div>
         <div id="footer-container" style={{ zIndex: '3' }}>
           <p id="footer-text-title">
-            HOW THIS WHITE LILY WAS RENDERED:
+            HOW THIS WHITE LILY IS RENDERED:
           </p>
           <div id="bottom-text-container">
             <div id="left-text-container" className="poster-text" style={{ zIndex: '3' }}>
@@ -82,16 +61,16 @@ export default function Home() {
                 presence of anthocyanins in the petals.
               </p>
             </div>
-            <p className="page-number">
-              02
-            </p>
+            {/* <p className="page-number">
+              01
+            </p> */}
           </div>
           <div id="footer">
             <p>
               FROM HOUSTON, TEXAS --- CURRENTLY IN DALLAS, TEXAS
             </p>
             <p>
-              PAGE INDEX 02
+              PAGE INDEX 01
             </p>
           </div>
         </div>

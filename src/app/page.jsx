265f5142e55bb72b1239fa6a7610789704716ee2
adamcesco <acescobedo@smu.edu>
2024-01-17 20:36:58 -0600

    second page design setup

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -3,7 +3,6 @@
 import React from 'react';
 
 import AsciiFlower from '@/widgets/AsciiCalc';
-import PageWindow from '@/widgets/PageWindow';
 import './page.css';
 
 export default function Home() {
@@ -12,7 +11,7 @@ export default function Home() {
       <AsciiFlower />
       <button type="button" id="contact-button">EMAIL</button>
       <div id="welcome-page">
-        <div id="welcome-text-div" style={{ zIndex: '1' }}>
+        <div id="welcome-text-div" style={{ zIndex: '3' }}>
           <h1 className="welcome-name" style={{ fontSize: '11.05rem', marginLeft: '-.04%' }}>
             ADAM
           </h1>
@@ -32,34 +31,70 @@ export default function Home() {
           width: '100vw',
           height: '3.3rem',
           backgroundColor: '#3d3d3d',
-          top: '-.5rem',
+          top: '-1vh',
         }}
       />
-      <div id="page-2">
-        <PageWindow
-          src=""
-          size="small"
-          tags={['', '', '']}
-          description=""
-          color=""
-          hoverText=""
-        />
-        <PageWindow
-          src=""
-          size="medium"
-          tags={['', '', '']}
-          description=""
-          color=""
-          hoverText=""
-        />
-        <PageWindow
-          src=""
-          size="large"
-          tags={['', '', '']}
-          description=""
-          color=""
-          hoverText=""
-        />
+      <div id="second-page">
+        <div id="title-container" style={{ zIndex: '3' }}>
+          <p className="title">
+            SOFTWARE ENGINEER
+          </p>
+          <p className="title">
+            EX-AMAZON INTERN
+          </p>
+          <p className="title">
+            EX-AT&amp;T INTERN
+          </p>
+          <p className="title">
+            PENDING-PATENT HOLDER
+          </p>
+          <p className="title">
+            SOUTHERN METHODIST UNIVERSITY
+          </p>
+          <p className="title">
+            BACHELOR OF SCIENCE
+          </p>
+          <p className="title">
+            COMPUTER SCIENCE
+          </p>
+          <p className="title">
+            2024 GRADUATE
+          </p>
+        </div>
+        <div id="footer-container" style={{ zIndex: '3' }}>
+          <p id="footer-text-title">
+            HOW THIS WHITE LILY WAS RENDERED:
+          </p>
+          <div id="bottom-text-container">
+            <div id="left-text-container" className="poster-text" style={{ zIndex: '3' }}>
+              <p>
+                White Lily model is rendered by Three.js, a WebGL renderer.
+                The model is a 3D scan of a White Lily flower blooming over
+                the course of three months. The model is rendered with a custom
+                shader that uses the position and color of a cell of pixels to
+                determine what ASCII characters are rendered.
+              </p>
+              <p>
+                The White Lily is a flower native to the Balkans and the Middle East.
+                It is a perennial plant that prefers full sun and well-drained soil.
+                It is a flower that is often used in religious ceremonies and is a
+                symbol of purity and innocence. It&apos;s purple color is due to the
+                presence of anthocyanins in the petals.
+              </p>
+            </div>
+            <p className="page-number">
+              02
+            </p>
+          </div>
+          <div id="footer">
+            <p>
+              FROM HOUSTON, TEXAS --- CURRENTLY IN DALLAS, TEXAS
+            </p>
+            <p>
+              PAGE INDEX 02
+            </p>
+          </div>
+        </div>
       </div>
     </>
   );
--- src/widgets/MenuHeader/MenuHeader.jsx
+++ src/widgets/MenuHeader/MenuHeader.jsx
@@ -1,27 +1,9 @@
 import React from 'react';
+import './MenuHeader.css';
 
 function MenuHeader() {
   return (
-    <button
-      type="button"
-      style={{
-        fontSize: 'medium',
-        background: 'none',
-        cursor: 'pointer',
-        position: 'fixed',
-        zIndex: '4',
-        top: '4%',
-        left: '4%',
-        padding: '.5rem .9rem .5rem .9em',
-        borderRadius: '2rem',
-        fontWeight: '400',
-        fontFamily: "'Segoe UI', Tahoma, Geneva, Verdana, sans-serif",
-        color: 'black',
-        borderStyle: 'solid',
-        borderWidth: '.05rem',
-        backgroundColor: '#fbf9f0',
-      }}
-    >
+    <button type="button" id="menu-button">
       MENU
     </button>
   );
